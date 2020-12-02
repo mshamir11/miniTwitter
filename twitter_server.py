cb251e1c6c6e48095516f12f68ec4cacb95326a1
mshamir11 <mshamir11@gmail.com>
2020-12-02 11:51:27 +0530

    completed login encryption

--- twitter_server.py
+++ twitter_server.py
@@ -7,6 +7,8 @@ import pandas as pd
 import datetime
 import sys
 import re
+import crypt
+from hmac import compare_digest as compare_hash
 
 #variables
 
@@ -14,7 +16,8 @@ import re
 '''
 Things to do
 
-  2. HashTags
+
+  3. Password encryption
   3. List of followers
 
 '''
@@ -38,6 +41,8 @@ USER_TO_ID =   'user_id.json'
 TWEET_TABLE = 'tweet_table.csv'
 HASHTAG = 'hashtag.json'
 HASHTAG_COUNT = 'hashtag.csv'
+ACTIVE_USERS ='active_users.csv'
+
 '''
 The name in the brackets is the respective function name
 Client Side View: 
@@ -124,7 +129,8 @@ def sendMessage(message,client_socket):
         print("Sending Error")
 
 def loginPage(client_socket):
-    sendMessage("Welcome To Mini-Twitter \n Choose the following: \n 1. New User \n 2. Existing User\n",client_socket)
+    print("login Page")
+    sendMessage("\n\nWelcome To Mini-Twitter \n Choose the following: \n 1. New User \n 2. Existing User\n",client_socket)
     user_response = client_socket.recv(10).decode()
     print(user_response)
     
@@ -136,24 +142,33 @@ def loginPage(client_socket):
 def newUser(client_socket):
     
     
-    user_database =open(USER_DATABASE, 'r+')
-    user_to_id = open(USER_TO_ID,'r+')
     
+    
+    user_database =open(USER_DATABASE, 'r+')
     user_database_dict = user_database.read()
     if len(user_database_dict)==0:
         
         
+        user_to_id = open(USER_TO_ID,'r+')
         sendMessage("Welcome New User \n please enter a user name: \n",client_socket)
         user_name = client_socket.recv(30).decode()
         sendMessage("Please Enter a password: ",client_socket)
         password = client_socket.recv(30).decode()
-        dictionary = {0:{'id_start':START_ID+1},START_ID+1:{'user_name':user_name,"password":password,"following":[],"followers":[]}}
+        crypted_password = crypt.crypt(password,crypt.METHOD_SHA512)
+        dictionary = {0:{'id_start':START_ID+1},START_ID+1:{'user_name':user_name,"password":crypted_password,"following":[str(START_ID+1)],"followers":[]}}
         user_database.write(json.dumps(dictionary,indent=4))
         user_id_dict ={user_name:START_ID+1}
         user_to_id.write(json.dumps(user_id_dict,indent=4))
-    
+        active_df = pd.read_csv(ACTIVE_USERS)
+        active_temp = pd.DataFrame({'user_id':[START_ID+1],'active':[0]})
+        active_df = active_df.append(active_temp,ignore_index=True)
+        active_df.to_csv(ACTIVE_USERS,header=True,index=False)
+        user_database.close()
+        user_to_id.close()
+            
     else:
         user_database =open(USER_DATABASE, 'r+')
+        user_to_id = open(USER_TO_ID,'r+')
         data =json.load(user_database)
         user_to_id_dict = json.load(user_to_id)
         sendMessage("Welcome New User \nPlease enter a user name: \n",client_socket)
@@ -169,7 +184,9 @@ def newUser(client_socket):
             except:
                   sendMessage("Please Enter a password: ",client_socket)
                   password = client_socket.recv(30).decode()
-                  data[data['0']['id_start']+1]={'user_name':user_name,"password":password,"following":[],"followers":[]}
+                  crypted_password = crypt.crypt(password,crypt.METHOD_SHA512)
+                  
+                  data[data['0']['id_start']+1]={'user_name':user_name,"password":crypted_password,"following":[str(data['0']['id_start']+1)],"followers":[]}
                   user_to_id_dict[user_name]=data['0']['id_start']+1
                   data['0']['id_start']=data['0']['id_start']+1
                   user_database.close()
@@ -180,12 +197,16 @@ def newUser(client_socket):
                   user_to_id.write(json.dumps(user_to_id_dict,indent=4))
                   user_to_id.close()
                   user_database.close()
+                  active_df = pd.read_csv(ACTIVE_USERS)
+                  active_temp = pd.DataFrame({'user_id':[int(data['0']['id_start'])],'active':[0]})
+                  active_df = active_df.append(active_temp,ignore_index=True)
+                  active_df.to_csv(ACTIVE_USERS,header=True,index=False)
                   loginPage(client_socket)
                   break
                  
         print("Im here")
 
-def feeds(client_sockt,user_id):
+def feeds(client_socket,user_id):
     print("feeds")
     
     tweet_table_df = pd.read_csv(TWEET_TABLE)
@@ -200,19 +221,19 @@ def feeds(client_sockt,user_id):
     message=""
     for i in range(len(df)):
         user_id_temp = df.iloc[i]['user_id']
-        message += f"{i+1}. {df.iloc[i]['content']} \n By : {data[user_id_temp]['user_name']} \n Posted On: {df.iloc[i]['last_update_time']} \n"
+        message += f"{i+1}. {df.iloc[i]['content']} \n By : {data[str(user_id_temp)]['user_name']} \n Posted On: {df.iloc[i]['last_update_time']} \n"
 
     message += "\n Press 1 to go back to the home page"
-    sendMessage(message,client_sockt)
-    response = client_sockt.recv(10).decode()
+    sendMessage(message,client_socket)
+    response = client_socket.recv(10).decode()
     
     if response=='1':
-        homePage(client_sockt,user_id)
+        homePage(client_socket,user_id)
         
     
-def postTweet(client_sockt,user_id):
-    sendMessage("Please type your tweet to be posted",client_sockt)
-    tweet = client_sockt.recv(10000).decode()
+def postTweet(client_socket,user_id):
+    sendMessage("Please type your tweet to be posted",client_socket)
+    tweet = client_socket.recv(10000).decode()
     tweet_table_df = pd.read_csv(TWEET_TABLE)
     hash_tag_df = pd.read_csv(HASHTAG_COUNT)
     
@@ -244,13 +265,12 @@ def postTweet(client_sockt,user_id):
         tweet_hashtag =open(HASHTAG, 'r+')
         hashtag_dic = json.load(tweet_hashtag)
         for item in hashtag_set:
-            if (hashtag_dic[item]):
+            if item in hashtag_dic.keys():
                 hashtag_dic[item]['tweet_ids'].append(str(tweet_id))
                 hashtag_dic[item]['tweet_count'] = int(hashtag_dic[item]['tweet_count'])+1
                 hash_tag_df.loc[hash_tag_df['hashtag'] == item, ['count']] +=1
             else:
-                hashtag_dic[item]['tweet_ids']=[str(tweet_id)]
-                hashtag_dic[item]['tweet_count'] ="1"
+                hashtag_dic[item]={'tweet_count':"1","tweet_ids":[str(tweet_id)]}
                 hash_tag_df_temp = pd.DataFrame({'hashtag':[item],'count':[1]})
                 hash_tag_df =hash_tag_df.append(hash_tag_df_temp,ignore_index=True)
         
@@ -260,16 +280,16 @@ def postTweet(client_sockt,user_id):
     
     tweet_hashtag.close()
     
-    sendMessage("Tweet posted. \n 1. Home page. \n 2. Post Another tweet \n 3. Quit ",client_sockt)
-    response = client_sockt.recv(30).decode()
+    sendMessage("Tweet posted. \n 1. Home page. \n 2. Post Another tweet \n 3. Quit ",client_socket)
+    response = client_socket.recv(30).decode()
     
     if response=='1':
-        homePage(client_sockt,user_id)
+        homePage(client_socket,user_id)
     elif response =='2':
-       postTweet(client_sockt,user_id)
+       postTweet(client_socket,user_id)
     
     elif response=='3':
-        logOut(client_sockt) 
+        logOut(client_socket) 
         
 def searchByName(client_socket,user_id):
     print("search by name")
@@ -365,7 +385,7 @@ def listOfFollowers(client_socket,target_user_id,target_user_name,client_user_id
     
 def individualUser(client_socket,target_user_id,target_user_name,client_user_id):
     print("Individual User")
-    sendMessage(f"Welcome to {target_user_name}'s page. \n 1. Feeds \n 2. Follow this user. \n 3. List of followers",client_socket)
+    sendMessage(f"\n\nWelcome to {target_user_name}'s page. \n 1. Feeds \n 2. Follow this user. \n 3. List of followers \n0. Home Page ",client_socket)
     response = client_socket.recv(10).decode()
     print(response)
     if response=='1':
@@ -375,7 +395,10 @@ def individualUser(client_socket,target_user_id,target_user_name,client_user_id)
         followUser(client_socket,target_user_id,target_user_name,client_user_id)
     
     elif response=='3':
-        listOfFollowers(client_socket,target_user_id,target_user_name,client_user_id) 
+        listOfFollowers(client_socket,target_user_id,target_user_name,client_user_id)
+    
+    else:
+        homePage(client_socket,client_user_id) 
     
 def listOfUsers(client_socket,client_user_id):
     print("List of users")
@@ -389,57 +412,99 @@ def listOfUsers(client_socket,client_user_id):
     response = client_socket.recv(10).decode()
     for i,key in enumerate(user_to_id_dict.keys()):
         if response==str(i+1):
-            individualUser(client_socket,user_to_id_dict[key],key,client_user_id)
+            individualUser(client_socket,str(user_to_id_dict[key]),key,client_user_id)
             break
 
-def searchPeople(client_sockt,user_id):    
+def searchPeople(client_socket,user_id):    
     print ("Search People")
-    sendMessage("1. List of registered users. \n 2. Search for user by name",client_sockt)
-    response = client_sockt.recv(30).decode()
+    sendMessage("1. List of registered users. \n 2. Search for user by name",client_socket)
+    response = client_socket.recv(30).decode()
     
     if response=='1':
-        listOfUsers(client_sockt,user_id)
+        listOfUsers(client_socket,user_id)
     elif response =='2':
-       searchByName(client_sockt,user_id)
+       searchByName(client_socket,user_id)
     
-def chat(client_sockt,user_id):      
+def activeUsersList(client_socket,user_id):
+    
+    user_database = open(USER_DATABASE,'r+')
+    user_database_dict = json.load(user_database)
+    follower_list = user_database_dict[user_id]['following']
+
+    active_df = pd.read_csv(ACTIVE_USERS)
+    df_new= pd.DataFrame()
+    for item in follower_list:
+   
+        df_temp =active_df[active_df['user_id']==int(item)] #the user id is in int. active is also in int
+        df_new = df_new.append(df_temp,ignore_index=True)
+
+    df_new = df_new[df_new['active']==1]
+    message ="\n\n"
+    if len(df_new)>0:
+        for i in range(len(df_new)):
+            message += f"{i+1}. {df_new.iloc[i]['user_id']}\n"
+    else:
+        message += "No one is online now. Check back later"
+    message += f"\n\n Press 1 to go back to previous menu, 2 for home page."
+    sendMessage(message,client_socket)
+    response = client_socket.recv(10).decode()
+    if response=='1':
+        chat(client_socket,user_id)
+    elif response=='2':
+        homePage(client_socket,user_id)
+
+def chat(client_socket,user_id): 
+         
     print("Chat")
+    sendMessage("\n\n1. List of active users \n 2. Chat with any follower.",client_socket)
+    response = client_socket.recv(10).decode()
+    
+    if response=='1':
+        activeUsersList(client_socket,user_id)
+    elif response=='2':
+        print("Chat with any follower")
+    else:
+        homePage(client_socket,user_id)
       
-def logOut(client_sockt):
+def logOut(client_socket,user_id):
     print("Log out page")
-    sendMessage("You have been successfull Logged out.\n 1. Login Page \n 2. Quit",client_sockt)
-    response = client_sockt.recv(30).decode()
+    active_df = pd.read_csv(ACTIVE_USERS)
+    active_df.loc[active_df['user_id']==int(user_id),['active']]=0
+    active_df.to_csv(ACTIVE_USERS,header=True,index=False)
     
+    sendMessage("You have been successfull Logged out.\n 1. Login Page \n 2. Quit",client_socket)
+    response = client_socket.recv(30).decode()
+   
     if response=='1':
-        loginPage(client_sockt)
+        loginPage(client_socket)
     
     elif response=='2':
-        client_sockt.close()
+        client_socket.close()
              
-def homePage(client_sockt,user_id):
+def homePage(client_socket,user_id):
     print("Im at home page")        
-    sendMessage("Home Page \n1. Feeds\n2. Post a tweet\n3. Search People\n4. Chat \n5. Trending Hashtags\n8. Logout ",client_sockt)
-    response = client_sockt.recv(30).decode()
+    sendMessage("Home Page \n1. Feeds\n2. Post a tweet\n3. Search People\n4. Chat \n5. Trending Hashtags\n8. Logout ",client_socket)
+    response = client_socket.recv(30).decode()
     
     if response=='1':
-        feeds(client_sockt,user_id)
+        feeds(client_socket,user_id)
     
     elif response=='2':
-        postTweet(client_sockt,user_id)
+        postTweet(client_socket,user_id)
     
     elif response=='3':
-        searchPeople(client_sockt,user_id)
+        searchPeople(client_socket,user_id)
     
     elif response=='4':
-        chat(client_sockt,user_id)
+        chat(client_socket,user_id)
     
     elif response=='5':
-        hashtags(client_sockt,user_id)
+        hashtags(client_socket,user_id)
     
     elif response=='8':
-        logOut(client_sockt)            
+        logOut(client_socket,user_id)            
     else:
-        homePage(client_sockt,user_id)
+        homePage(client_socket,user_id)
     
 def existingUser(client_socket):
     
@@ -457,12 +522,18 @@ def existingUser(client_socket):
     while True:
         
     #  try: 
-        if data[str(user_to_id_dict[user_name])]['password']==password :
+        if compare_hash(data[str(user_to_id_dict[user_name])]['password'], crypt.crypt(password, data[str(user_to_id_dict[user_name])]['password'])): 
+       
+            active_df = pd.read_csv(ACTIVE_USERS)
+            active_df.loc[active_df['user_id']==user_to_id_dict[user_name],['active']]=1
+            active_df.to_csv(ACTIVE_USERS,header=True,index=False)
             homePage(client_socket,str(user_to_id_dict[user_name]))
+
             break
         else:
             sendMessage("Incorrect Password. Please re-enter correct the password",client_socket)
             password = client_socket.recv(30).decode()
+            crypted_password= crypt.crypt(password,crypt.METHOD_SHA512)
                 
             
   
